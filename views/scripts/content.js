4520bcb78338aced2bcc7f7ae7efc449cae14f8a
Tom Ware <tomjware@gmail.com>
2021-12-03 20:04:48 -0500

    Changed button HTML to variables to move to external JS file later

--- views/scripts/content.js
+++ views/scripts/content.js
@@ -4,6 +4,18 @@ const db = firebase.firestore();
 const login = db.collection('users').doc('logins');
 const timestamp = db.collection('users').doc('timestamp');
 
+var adminContent = [`
+  <div align='center' onclick='window.location.href = "./messages/index.html";'>Message Board</div>
+  <div align='center' onclick='window.location.href = "./viewer/index.html";'>Schedule Viewer</div>
+  <div align='center' onclick='window.location.href = "./schedule/index.html";'>Schedule Editor</div>
+  <div align='center' onclick='window.location.href = "./commands/index.html";'>TGSNBot Commands</div>
+  <div align='center' onclick='logout()'>Logout</div>`]
+var baseContent = [`
+  <div align='center' onclick='window.location.href = "./messages/index.html";'>Message Board</div>
+  <div align='center' onclick='window.location.href = "./viewer/index.html";'>Schedule Viewer</div>
+  <div align='center' onclick='window.location.href = "./commands/index.html";'>TGSNBot Commands</div>
+  <div align='center' onclick='logout()'>Logout</div>`]
+
 function init(){
   var content = document.getElementById('mainContent');
   var loggedIn = localStorage.getItem('loggedIn');
@@ -11,18 +23,9 @@ function init(){
 
   if (loggedIn == 'yes'){
     if (position == 'Network Admin' || position == 'TGSN Coordinator'){
-    content.innerHTML = [`
-      <div align='center' onclick='window.location.href = "./messages/index.html";'>Message Board</div>
-      <div align='center' onclick='window.location.href = "./viewer/index.html";'>Schedule Viewer</div>
-      <div align='center' onclick='window.location.href = "./schedule/index.html";'>Schedule Editor</div>
-      <div align='center' onclick='window.location.href = "./commands/index.html";'>TGSNBot Commands</div>
-      <div align='center' onclick='logout()'>Logout</div>`]
+    content.innerHTML = adminContent
     } else {
-      content.innerHTML = [`
-        <div align='center' onclick='window.location.href = "./messages/index.html";'>Message Board</div>
-        <div align='center' onclick='window.location.href = "./viewer/index.html";'>Schedule Viewer</div>
-        <div align='center' onclick='window.location.href = "./commands/index.html";'>TGSNBot Commands</div>
-        <div align='center' onclick='logout()'>Logout</div>`]
+      content.innerHTML = baseContent
     }
   } else {
     content.innerHTML = [`

0e60d0362c090bb9f28cea39427691f36f24cd25
Tom Ware <tomjware@gmail.com>
2021-12-29 15:27:22 -0500

    Added New Button

--- views/messages/index.js
+++ views/messages/index.js
@@ -8,6 +8,33 @@ db.collection('app').doc('data').onSnapshot((doc) => {
   id = doc.data().id;
 })
 
+function showMBL(){
+  var modal = document.getElementById('mbModal');
+  var span = document.getElementsByClassName('close')[1];
+
+  modal.style.display = 'block';
+  span.onclick = function(){
+    modal.style.display = 'none';
+  }
+}
+
+function addLinkImage(type){
+  const chatTxt = document.getElementById('chat-txt');
+  var message = chatTxt.value;
+  var modal = document.getElementById('mbModal');
+  var link = document.getElementById('messageBoardLink').value;
+
+  if (type == 'link'){
+    message = message + "[url='" + link + "']" + link + "[/url]"
+  }
+  if (type == 'image'){
+    message = message + "[img]" + link + "[/img]"
+  }
+  chatTxt.value = message;
+
+  modal.style.display = 'none';
+}
+
 function postChat(){
   //e.preventDefault();
   const date = new Date().toLocaleString();
@@ -22,7 +49,7 @@ function postChat(){
     "0": [date, msgFB]
   })*/
 
-  db.collection('app').doc('main').update({
+  /*db.collection('app').doc('main').update({
     [id]: [date, msgFB]
   })
   id = parseInt(id + 1);
@@ -35,7 +62,9 @@ function postChat(){
   var params = {
       content: msgDiscord
   }
-  request.send(JSON.stringify(params));
+  request.send(JSON.stringify(params));*/
+
+  document.querySelector("#chat-txt").style.height = 'auto';
 }
 
 //Listen for updates
